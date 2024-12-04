0ccd5590b6f83eccd69e096e685482bf301a7899
synksta <contriturbamentum@gmail.com>
2024-12-04 13:42:13 +0700

    add suite and docstrings, change test names

---
+++ suite.py
@@ -0,0 +1,35 @@
+import unittest
+from test_page_load import TestPageLoad
+from test_links import TestLinks
+from test_login import TestLogin
+from test_change_password import TestChangePassword
+from test_favorites import TestFavorites
+
+# Список всех классов тестов
+test_classes = [
+    TestPageLoad,
+    TestLinks,
+    TestLogin,
+    TestChangePassword,
+    TestFavorites,
+]
+
+
+def suite():
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+
+    for test_class in test_classes:
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+
+        # Вывод информации о каждом тесте
+        for test in tests:
+            print(f"Test ID: {test.id()}, Description: {test.shortDescription()}")
+
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
